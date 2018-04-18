67e50165f4cce6a9f6fcc07539876ea3f3ecce70
Happyrobot33 <32679887+Happyrobot33@users.noreply.github.com>
2018-04-18 09:14:32 -0400

    Add Conveyor prefab script

---
+++ Code/Conveyor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour {
+    //Speed of the conveyor
+    public float speed = 1;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //When objects enter the conveyors range of effect
+    void OnTriggerEnter(Collider other)
+    {
+        rb = other.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+    }
+}
